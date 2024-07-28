300a45f47c79bf7bab5e424e4c57d2578fb2b21c
alstdu <lexi.dugo@gmail.com>
2024-07-28 14:58:32 -0700

    Update styling, form functionality, and navigation

--- Components/CreateAccount.js
+++ Components/CreateAccount.js
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
-import { View, Text, ImageBackground, StyleSheet, Button, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Button, TextInput, TouchableOpacity } from 'react-native';
 
-const image = '../assets/loginBackground.png';
-
-export default function CreateAccount() {
+export default function CreateAccount({ navigation }) {
     const {
         control,
         handleSubmit,
@@ -18,9 +16,11 @@ export default function CreateAccount() {
     const onSubmit = (data) => console.log(data)
     return (
         <View style={styles.container}>
-            <ImageBackground source={require(image)} resizeMode="stretch" style={styles.image}>
-                <Text>Login Account</Text>
-                <Text>Welcome back to Get That Meow</Text>
+            <View style={styles.formContainer}>
+                <View style={styles.circle}></View>
+                <Text>Create Account</Text>
+                <Text>Let's get started!</Text>
+                <Text>OR</Text>
                 <Controller
                     control={control}
                     rules={{
@@ -36,12 +36,13 @@ export default function CreateAccount() {
                     )}
                     name="emailInput"
                 />
-                {errors.emailInput && <Text>This is required.</Text>}
+                {errors.emailInput && <Text>Email is required.</Text>}
 
                 <Controller
                     control={control}
                     rules={{
                         maxLength: 100,
+                        required: true,
                     }}
                     render={({ field: { onChange, onBlur, value } }) => (
                         <TextInput
@@ -53,23 +54,44 @@ export default function CreateAccount() {
                     )}
                     name="passwordInput"
                 />
+                {errors.passwordInput && <Text>Password is required.</Text>}
 
-                <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+                <TouchableOpacity onPress={handleSubmit(onSubmit)}><Text>Create</Text></TouchableOpacity>
                 <View>
-                    <Text>New here?</Text>
-                    <TouchableOpacity><Text>Create an Account</Text></TouchableOpacity>
+                    <Text>Already Registered?</Text>
+                    <TouchableOpacity onPress={() =>
+                        navigation.navigate("Home")}><Text>Log-in</Text></TouchableOpacity>
                 </View>
-            </ImageBackground>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
         flex: 1,
+        backgroundColor: '#F3DAE2',
+    },
+    formContainer: {
+        backgroundColor: "white",
+        height: "75%",
+        width: "75%",
+        padding: 25,
+        borderRadius: 30,
     },
     image: {
         flex: 1,
         justifyContent: 'center',
+    },
+    circle: {
+        width: 60,
+        height: 60,
+        borderRadius: 60 / 2,
+        backgroundColor: "#D05F86",
+        marginTop: -60, //I'm aware this is cursed but...
+        marginLeft: 93,
     },
 });
