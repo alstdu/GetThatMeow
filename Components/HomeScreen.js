583a62e8bc2152dd99324c7ba1a091e0737dbeb7
alstdu <lexi.dugo@gmail.com>
2024-07-29 11:28:57 -0700

    Fix background color

--- Components/HomeScreen.js
+++ Components/HomeScreen.js
@@ -77,6 +77,7 @@ export default function HomeScreen({ navigation }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: "white",
     },
     innerContainer: {
         display: 'flex',
