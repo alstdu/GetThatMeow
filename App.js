2e9158173bad62fc63322f5b89e6aaf0d7aaab78
alstdu <lexi.dugo@gmail.com>
2024-07-28 22:31:30 -0700

    Add logo to header

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image } from 'react-native';
 import HomeScreen from './Components/HomeScreen';
 import CreateAccount from './Components/CreateAccount';
 import CreateProfile from './Components/CreateProfile';
 
 const Stack = createNativeStackNavigator();
 
+function LogoTitleWhite() {
+  return (
+    <Image
+      style={{ width: 90, height: 70, alignContent: "center" }}
+      source={require('./assets/logoWhite.png')}
+    />
+  );
+}
+
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="CreateAccount" component={CreateAccount} />
-        <Stack.Screen name="CreateProfile" component={CreateProfile} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
+        <Stack.Screen name="CreateAccount" component={CreateAccount} options={{headerShown: false}} />
+        <Stack.Screen name="CreateProfile" component={CreateProfile} options={{
+           headerStyle: {
+            backgroundColor: '#E66264',
+          },
+          headerTintColor: '#FFF',
+          headerTitle: (props) => <LogoTitleWhite {...props} />, headerTitleAlign: 'center', 
+        }}
+          />
       </Stack.Navigator>
     </NavigationContainer>
   );
